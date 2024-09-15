1ab65c0af8de2a0a553b52e30f4d9db3c30aae10
Mitchell Murphy <mitchell.murphy@darkwolfsolutions.com>
2024-09-14 21:06:43 -0400

    Updated cluster_msh.sh script and cilium-values.yaml.j2; added rebel-base manifests for testing clustermesh

--- scripts/cluster_mesh.sh
+++ scripts/cluster_mesh.sh
@@ -1,28 +1,26 @@
 #!/usr/bin/env bash
 
-# cluster_mesh.sh --create-ca --context1 <context1> --context2 <context2> --enable-mesh
-
 usage() {
-  echo "Usage: $0 [--create-ca] [--context1 <context1>] [--context2 <context2>] [--enable-mesh]"
+  echo "Usage: $0 [--sync-ca] [--contexts <context1,context2,...>] [--enable-mesh] [--mesh-service-type <service-type>]"
   exit 1
 }
 
 extract_args() {
   while [ $# -gt 0 ]; do
     case $1 in
-      --create-ca)
-        is_configured_ca=false
+      --sync-ca)
+        SYNC_CA=true
         ;;
-      --context1)
+      --contexts)
         shift
-        CLUSTER1=$1
-        ;;
-      --context2)
-        shift
-        CLUSTER2=$1
+        CONTEXTS=$1
         ;;
       --enable-mesh)
-        enable_mesh=true
+        ENABLE_MESH=true
+        ;;
+      --mesh-service-type)
+        shift
+        MESH_SERVICE_TYPE=$1
         ;;
       *)
         usage
@@ -33,24 +31,46 @@ extract_args() {
 }
 
 main() {
+  declare -A CLUSTERS
   # if --help print usage
   if [ "$1" = "--help" ]; then
     usage
   fi
   extract_args "$@"
-  if [ "$is_configured_ca" = false ]; then
-    kubectl --context=$CLUSTER2 delete secret -n kube-system cilium-ca
-    kubectl --context=$CLUSTER1 get secret -n kube-system cilium-ca -o yaml | \
-      kubectl --context $CLUSTER2 create -f -
+
+  # Set IFS to comma (,) to split the string
+  IFS=',' read -r -a context_array <<< "$CONTEXTS"
+
+  # print the context_array
+  echo "Contexts: ${context_array[@]}"
+
+  if [ "$SYNC_CA" = true ]; then
+    # get first context
+    c1=${context_array[0]}
+    # get length of context_array
+    len=${#context_array[@]}
+    # for every cluster except the first one
+    for (( i=1; i<$len; i++ )); do
+      c2=${context_array[$i]}
+      kubectl delete secret -n kube-system cilium-ca --context=$c2
+      kubectl get secret -n kube-system cilium-ca -o yaml --context=$c1 | \
+        kubectl create -f - --context=$c2
+    done
   fi
 
   if [ "$enable_mesh" = true ]; then
-    cilium clustermesh enable --context $CLUSTER1 --service-type ClusterIP
-    cilium clustermesh enable --context $CLUSTER2 --service-type ClusterIP
+    # enable clustermesh on each cluster
+    for context in "${context_array[@]}"; do
+      cilium clustermesh enable --context $context --service-type $MESH_SERVICE_TYPE
+    done
   fi
 
-  cilium clustermesh connect --context $CLUSTER1 --destination-context $CLUSTER2
-  cilium clustermesh connect --context $CLUSTER2 --destination-context $CLUSTER1
+  for context in "${context_array[@]}"; do
+    # need to connect cluster1 with every other cluster
+    c1=${context_array[0]}
+    for (( i=1; i<$len; i++ )); do
+      cilium clustermesh connect --context $c1 --destination-context ${context_array[$i]}
+  done
 }
 
 main "$@"
